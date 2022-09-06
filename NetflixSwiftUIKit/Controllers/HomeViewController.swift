c6ce442217274b941c70a773f621a0bdf17dedf3
aidoka22 <aidana.sharipbay@gmail.com>
2022-09-06 21:45:06 +0600

    Upcoming page finished

--- NetflixSwiftUIKit/Controllers/HomeViewController.swift
+++ NetflixSwiftUIKit/Controllers/HomeViewController.swift
@@ -7,9 +7,17 @@
 
 import UIKit
 
+enum Sections : Int {
+    case TrendingMovies = 0
+    case TrendingTv = 1
+    case Popular = 2
+    case UpcomingMovies = 3
+    case TopRated = 4
+}
+
 class HomeViewController: UIViewController {
 
-    let sectionTitles : [String] = ["Trending Movies" , "Popular" , "Trending TV" , "Upcoming Movies" , "Top rated"]
+    let sectionTitles : [String] = ["Trending Movies" ,"Trending TV" , "Popular"  , "Upcoming Movies" , "Top rated"]
     let backgroundColor = UIColor { tc in
         switch tc.userInterfaceStyle {
         case .dark:
@@ -40,8 +48,6 @@ class HomeViewController: UIViewController {
         
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
-        
-        getTrendingMovies()
     }
     
     private func configureNavbar(){
@@ -62,19 +68,9 @@ class HomeViewController: UIViewController {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
     }
-    
-    private func getTrendingMovies (){
-        APICaller.shared.getTrendingMovies { results in
-            switch results {
-            case .success(let movies):
-                print(movies)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
 }
+
+//MARK: - UITableViewDelegate & UITableViewDataSource
 
 extension HomeViewController : UITableViewDelegate , UITableViewDataSource {
     
@@ -89,6 +85,55 @@ extension HomeViewController : UITableViewDelegate , UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath)
                 as? CollectionViewTableViewCell else{
+            return UITableViewCell()
+        }
+        switch indexPath.section {
+        case Sections.TrendingMovies.rawValue :
+            APICaller.shared.getTrendingMovies { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        case Sections.TrendingTv.rawValue :
+            APICaller.shared.getTrendingTvs { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        case Sections.Popular.rawValue :
+            APICaller.shared.getPopular { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        case Sections.UpcomingMovies.rawValue :
+            APICaller.shared.getUpcomingmovies { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        case Sections.TopRated.rawValue :
+            APICaller.shared.getTopRated { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        default :
             return UITableViewCell()
         }
         return cell
---
+++ NetflixSwiftUIKit/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModle.swift
+//  NetflixSwiftUIKit
+//
+//  Created by Айдана Шарипбай on 06.09.2022.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName   : String
+    let posterURL : String
+}
